2563410b1319caaa5ca2c1d68114614310468ef7
ControlSystem <controlsystem88@hotmail.com>
2018-10-27 13:49:48 -0500

    js Actualizado

--- assets/js/Peliculas.js
+++ assets/js/Peliculas.js
@@ -1,33 +1,37 @@
-// window.onload=function(){//Esta funcion se utiliza para inicializar las funciones principales antes del codigo.
-//     let total = document.getElementById('total');
-//     total = 'se han encontrado ${items} peliculas'
-//     let items = document.getElementsByTagName('items').length;
-// }
+window.onload=function(){//Esta funcion se utiliza para inicializar las funciones principales antes del codigo.
+    let numeros = document.getElementById('numeros');
+    numeros = 'se han encontrado ${items} peliculas'
+    let items = document.getElementsByTagName('items').length;
+}
 
 function añadir(){//Funcion añadir
     //console.log('Hola');
 
 let Nombre = document.getElementById('nombre');//Declaracion de variable Nombre evaluada a su Id
+let NombreV = document.getElementById('nombre').value;
   //console.log(Nombre);
 let Genero = document.getElementById('genero');//Declaracion de variable Genero evaluada a su Id
+let GeneroV = document.getElementById('genero').value;
   //console.log('Genero');
 let Stock = document.getElementById('stock'); //Declaracion de variable Stock evaluada a su Id
+let StockV = document.getElementById('stock').value;
    //console.log('Stock');
 let Raiting = document.getElementById('raiting');//Declaracion de variable Raiting evaluada a su Id
+let RaitingV = document.getElementById('raiting').value;
    //console.log('raiting');
 
 //Condicional IF, si la variable nombre evaluada con la funcion (Trim) es diferente de (nada) se pintara de un color y se añadira un borde,
 if(Nombre.value.trim()==''){//pero si no, se le dara otro color.
-    console.log('Hola');
+    //console.log('Hola');
     Nombre.style.border ='1px solid red';
  }else{
-    console.log('Hola');
+    //console.log('Hola');
     Nombre.style.borderColor ='gray';
-    Nombre.style.backgroundColor = 'gray';
+    Nombre.style.backgroundColor = 'white';
  }
 
 if(Genero.value.trim()==''){
-    console.log('Hola');
+    //console.log('Hola');
     Genero.style.border='1px solid red';
 }else{
     Genero.style.borderColor ='gray';
@@ -35,7 +39,7 @@ if(Genero.value.trim()==''){
 }
 
 if(Stock.value.trim()==''){
-     console.log('Hola');
+     //console.log('Hola');
     Stock.style.border='1px solid red';
 }else{
     Stock.style.borderColor ='gray';
@@ -43,7 +47,7 @@ if(Stock.value.trim()==''){
 }
 
 if(Raiting.value.trim()==''){
-     console.log('Hola');
+     //console.log('Hola');
     Raiting.style.border='1px solid red';
 }else{
     Raiting.style.borderColor ='gray';
@@ -55,7 +59,7 @@ if(Raiting.value.trim()==''){
 // // console.log('tbody')
 
  let estrellas='';
- switch(Raiting){
+ switch(RaitingV){
      //Ciclo switch
      case '1':
      //Cada caso representa un suceso en los iconos.
@@ -84,15 +88,15 @@ if(Raiting.value.trim()==''){
  //Declaracion de Tr
  let Agregar = '';
  let fila;
- let items;
+ let items=0;
  let tabla = document.getElementById('tabla');
 
  if(Agregar = 1){
-    html = `<tr>
+    fila = `<tr>
  <td>${items+1}</td>
- <td>${Nombre}</td>
- <td>${Genero}</td>
- <td>${Stock}</td>
+ <td>${NombreV}</td>
+ <td>${GeneroV}</td>
+ <td>${StockV}</td>
  <td>${estrellas}</td>
  <td>
  <a href="pelicula-ficha.htm" class="btn btn-info btn-sm">
@@ -104,13 +108,16 @@ if(Raiting.value.trim()==''){
   </td>
   </tr>`
 
-  tr.innerHTML = fila;
-  tabla.appendChild('tr');
-  const myNotification = window.createNotification({});
-             // options here
-             myNotification({
-                 title: 'Listo', 
-                 message: 'La pelicula "Agregar ha sido agregada"'
-       });
+
+  document.getElementById("tabla").innerHTML += fila;
+//   tr.innerHTML = fila;
+//   tabla.appendChild('tr');
+//   const myNotification = window.createNotification({});
+//              // options here
+//              myNotification({
+//                  title: 'Listo', 
+//                  message: 'La pelicula "Agregar ha sido agregada"'
+//        });
+
      }
 }
